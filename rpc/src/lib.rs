9f3443082a9b25d546103a6dbec99793f33223e5
icodezjb <icodezjb@users.noreply.github.com>
2021-11-18 13:24:44 +0800

    Add eth api for rpc

--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -24,6 +24,20 @@ use chainx_runtime::{AccountId, Balance, BlockNumber, Hash, Index};
 use xpallet_mining_asset_rpc_runtime_api::MiningWeight;
 use xpallet_mining_staking_rpc_runtime_api::VoteWeight;
 
+// ETH-API
+use std::collections::BTreeMap;
+use sc_network::NetworkService;
+use sc_client_api::{
+    backend::StorageProvider, client::BlockchainEvents
+};
+use sc_transaction_pool::{ChainApi, Pool};
+use fc_rpc_core::types::FilterPool;
+use jsonrpc_pubsub::manager::SubscriptionManager;
+use pallet_ethereum::EthereumStorageSchema;
+use fc_rpc::{
+    StorageOverride, SchemaV1Override, OverrideHandle, RuntimeApiStorageOverride, EthBlockDataCache
+};
+
 /// Light client extra dependencies.
 pub struct LightDeps<C, F, P> {
     /// The client instance to use.
@@ -60,8 +74,25 @@ pub struct GrandpaDeps<B> {
     pub finality_provider: Arc<FinalityProofProvider<B, Block>>,
 }
 
+pub struct EthDeps<A: ChainApi> {
+    /// Graph pool instance.
+    pub graph: Arc<Pool<A>>,
+    /// The Node authority flag
+    pub is_authority: bool,
+    /// Whether to enable dev signer
+    pub enable_dev_signer: bool,
+    /// Network service
+    pub network: Arc<NetworkService<Block, Hash>>,
+    /// EthFilterApi pool.
+    pub filter_pool: Option<FilterPool>,
+    /// Backend.
+    pub backend: Arc<fc_db::Backend<Block>>,
+    /// Maximum number of logs in a query.
+    pub max_past_logs: u32,
+}
+
 /// Full client dependencies.
-pub struct FullDeps<C, P, SC, B> {
+pub struct FullDeps<C, P, SC, B, A: ChainApi> {
     /// The client instance to use.
     pub client: Arc<C>,
     /// Transaction pool instance.
@@ -76,14 +107,17 @@ pub struct FullDeps<C, P, SC, B> {
     pub babe: BabeDeps,
     /// GRANDPA specific dependencies.
     pub grandpa: GrandpaDeps<B>,
+    /// Eth api
+    pub eth: EthDeps<A>,
 }
 
 /// A IO handler that uses all Full RPC extensions.
 pub type IoHandler = jsonrpc_core::IoHandler<sc_rpc::Metadata>;
 
 /// Instantiate all Full RPC extensions.
-pub fn create_full<C, P, SC, B>(
-    deps: FullDeps<C, P, SC, B>,
+pub fn create_full<C, P, SC, B, A>(
+    deps: FullDeps<C, P, SC, B, A>,
+    subscription_task_executor: SubscriptionTaskExecutor
 ) -> Result<jsonrpc_core::IoHandler<sc_rpc_api::Metadata>, Box<dyn std::error::Error + Send + Sync>>
 where
     C: ProvideRuntimeApi<Block>
@@ -122,7 +156,10 @@ where
         BlockNumber,
     >,
     C::Api: xpallet_transaction_fee_rpc_runtime_api::XTransactionFeeApi<Block, Balance>,
-    P: TransactionPool + 'static,
+    P: TransactionPool<Block = Block> + Sync + Send + 'static,
+    C::Api: fp_rpc::EthereumRuntimeRPCApi<Block>,
+    A: ChainApi<Block = Block> + 'static,
+    C: StorageProvider<Block, B> + BlockchainEvents<Block>,
     SC: SelectChain<Block> + 'static,
     B: sc_client_api::Backend<Block> + Send + Sync + 'static,
     B::State: sc_client_api::backend::StateBackend<sp_runtime::traits::HashFor<Block>>,
@@ -136,6 +173,11 @@ where
     use xpallet_mining_asset_rpc::{XMiningAsset, XMiningAssetApi};
     use xpallet_mining_staking_rpc::{XStaking, XStakingApi};
     use xpallet_transaction_fee_rpc::{XTransactionFee, XTransactionFeeApi};
+    use fc_rpc::{
+        EthApi, EthApiServer, EthDevSigner, EthFilterApi, EthFilterApiServer, EthPubSubApi,
+        EthPubSubApiServer, EthSigner, HexEncodedIdProvider, NetApi, NetApiServer, Web3Api,
+        Web3ApiServer,
+    };
 
     let mut io = jsonrpc_core::IoHandler::default();
     let FullDeps {
@@ -146,6 +188,7 @@ where
         deny_unsafe,
         grandpa,
         babe,
+        eth,
     } = deps;
     let BabeDeps {
         keystore,
@@ -162,7 +205,7 @@ where
 
     io.extend_with(SystemApi::to_delegate(FullSystem::new(
         client.clone(),
-        pool,
+        pool.clone(),
         deny_unsafe,
     )));
     io.extend_with(TransactionPaymentApi::to_delegate(TransactionPayment::new(
@@ -209,7 +252,85 @@ where
     io.extend_with(XGatewayRecordsApi::to_delegate(XGatewayRecords::new(
         client.clone(),
     )));
-    io.extend_with(XGatewayCommonApi::to_delegate(XGatewayCommon::new(client)));
+    io.extend_with(XGatewayCommonApi::to_delegate(XGatewayCommon::new(client.clone())));
+
+    let EthDeps {
+        graph,
+        is_authority,
+        enable_dev_signer,
+        network,
+        filter_pool,
+        backend,
+        max_past_logs
+    } = eth;
+
+    let mut signers = Vec::new();
+    if enable_dev_signer {
+        signers.push(Box::new(EthDevSigner::new()) as Box<dyn EthSigner>);
+    }
+    let mut overrides_map = BTreeMap::new();
+    overrides_map.insert(
+        EthereumStorageSchema::V1,
+        Box::new(SchemaV1Override::new(client.clone()))
+            as Box<dyn StorageOverride<_> + Send + Sync>,
+    );
+
+    let overrides = Arc::new(OverrideHandle {
+        schemas: overrides_map,
+        fallback: Box::new(RuntimeApiStorageOverride::new(client.clone())),
+    });
+
+    let block_data_cache = Arc::new(EthBlockDataCache::new(50, 50));
+
+    io.extend_with(
+        EthApiServer::to_delegate(EthApi::new(
+            client.clone(),
+            pool.clone(),
+            graph,
+            chainx_runtime::TransactionConverter,
+            network.clone(),
+            signers,
+            overrides.clone(),
+            backend.clone(),
+            is_authority,
+            max_past_logs,
+            block_data_cache.clone()
+        ))
+    );
+
+    if let Some(filter_pool) = filter_pool {
+        io.extend_with(
+            EthFilterApiServer::to_delegate(EthFilterApi::new(
+                client.clone(),
+                backend,
+                filter_pool.clone(),
+                500 as usize, // max stored filters
+                overrides.clone(),
+                max_past_logs,
+                block_data_cache
+            ))
+        );
+    }
+
+    io.extend_with(NetApiServer::to_delegate(NetApi::new(
+        client.clone(),
+        network.clone(),
+        // Whether to format the `peer_count` response as Hex (default) or not.
+        true,
+    )));
+
+    io.extend_with(Web3ApiServer::to_delegate(Web3Api::new(client.clone())));
+
+    io.extend_with(EthPubSubApiServer::to_delegate(EthPubSubApi::new(
+        pool,
+        client,
+        network,
+        SubscriptionManager::<HexEncodedIdProvider>::with_id_provider(
+            HexEncodedIdProvider::default(),
+            Arc::new(subscription_task_executor),
+        ),
+        overrides,
+    )));
 
     Ok(io)
 }
